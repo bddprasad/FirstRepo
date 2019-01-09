b9a131a880338e50e51baac946286ff6f5268cbc
bddprasad <bddprasad555@gmail.com>
2019-01-09 12:34:26 -0500

    created a test1 testsuite

---
+++ Test Suites/test1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ec8c7d23-670e-452d-bb45-57bb1b2d9ed2</testSuiteGuid>
+</TestSuiteEntity>
