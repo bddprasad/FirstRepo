fe06869697fbec281aa771954b4b0ea22a661bf5
bddprasad <bddprasad555@gmail.com>
2019-01-09 12:35:11 -0500

    included test2

---
+++ Test Suites/test2.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>creating test2</description>
+   <name>test2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>445e76c1-5e62-43e2-bfae-11d87dfd8fbc</testSuiteGuid>
+</TestSuiteEntity>
